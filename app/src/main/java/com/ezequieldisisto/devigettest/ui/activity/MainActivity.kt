01a861cdc7269fa2db3c6174c3a6d3af9e21c48f
Ezequiel Di Sisto <endisisto@gmail.com>
2019-12-11 18:22:38 -0300

    create the fragments for the List and Detail screens, add navigation between them

--- app/src/main/java/com/ezequieldisisto/devigettest/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ezequieldisisto/devigettest/ui/activity/MainActivity.kt
@@ -3,11 +3,14 @@ package com.ezequieldisisto.devigettest.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ezequieldisisto.devigettest.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
     }
 }
---
+++ app/src/main/java/com/ezequieldisisto/devigettest/ui/fragment/DetailFragment.kt
@@ -0,0 +1,15 @@
+package com.ezequieldisisto.devigettest.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ezequieldisisto.devigettest.R
+
+class DetailFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+}
